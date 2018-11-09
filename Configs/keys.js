df84f6032b518ceb657e4f2665d9f124464fb973
juliof9 <00081817@uca.edu.sv>
2018-11-09 08:24:41 -0600

    Terminando

---
+++ Configs/keys.js
@@ -0,0 +1,8 @@
+//credenciales de conexion a la base de datos
+                        module.exports = {
+                            mongodb:
+                            {
+                                URI: 'mongodb://localhost:27017/practica' 
+                            }
+                        }
+                        
